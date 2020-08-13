2215452d44af97d11557a302f69abc93d71257d4
Felipe Fernandes <feelipefernandes@live.com>
2020-08-13 20:02:36 -0300

    Create contanucleotideos.py

---
+++ Bioinformática/contanucleotideos.py
@@ -0,0 +1,25 @@
+#Lê um arquivo FASTA e retorna 4 números inteiros (separados por espaço) contando o respectivo número de vezes que os símbolos 'A', 'C', 'G', and 'T' aparecem.
+
+#!/usr/bin/env python
+# coding: utf-8
+
+from Bio import SeqIO
+
+A=0 
+C=0 
+G=0
+T=0
+
+for record in SeqIO.parse("covid19.fasta", "fasta"):
+    
+    for c in record.seq:
+        if c == 'A':
+            A+=1
+        if c == 'C':
+            C+=1
+        if c == 'G':
+            G+=1
+        if c == 'T':
+            T+=1
+    
+print("A:",A, "C:", C, "G:", G, "T:", T)
